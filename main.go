9bd20075e9ad0bfe54b2fbb3222761ebf2a1026a
Fang-Pen Lin <hello@fangpenlin.com>
2023-06-17 00:37:20 -0700

    Read annotations

--- main.go
+++ main.go
@@ -5,13 +5,56 @@ import (
 	spec "github.com/opencontainers/runtime-spec/specs-go"
 	"log"
 	"os"
+	"path"
+	"strings"
 )
+
+const (
+	annotationPrefix string = "com.launchplatform.oci-hooks.archive-overlay."
+)
+
+type Archive struct {
+	Src  string
+	Dest string
+}
 
 func main() {
 	var state spec.State
 	err := json.NewDecoder(os.Stdin).Decode(&state)
 	if err != nil {
 		log.Fatal(err)
+	}
+	jsonFile, err := os.Open(path.Join(state.Bundle, "config.json"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	var config spec.Spec
+	err = json.NewDecoder(jsonFile).Decode(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	archives := map[string]Archive{}
+	for key, value := range config.Annotations {
+		if !strings.HasPrefix(key, annotationPrefix) {
+			continue
+		}
+		keySuffix := key[len(annotationPrefix):]
+		parts := strings.Split(keySuffix, ".")
+		archiveKey, archiveArg := parts[0], parts[1]
+		archive, ok := archives[archiveKey]
+		if !ok {
+			archive = Archive{}
+		}
+		if archiveArg == "src" {
+			archive.Src = value
+		} else if archiveArg == "dest" {
+			archive.Dest = value
+		} else {
+			log.Printf("Invalid archive argument %s\n", archiveArg)
+			continue
+		}
+		archives[archiveKey] = archive
 	}
 
 }
