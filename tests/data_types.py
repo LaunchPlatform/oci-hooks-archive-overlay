aa2800ba2d053d6459721fa87373b0b49e0bfa50
Fang-Pen Lin <hello@fangpenlin.com>
2023-06-30 18:25:18 -0700

    Try to make test works

--- tests/data_types.py
+++ tests/data_types.py
@@ -1,13 +1,13 @@
+import dataclasses
 import typing
 
 from containers import ImageMount
 from containers.data_types import PathType
 
 
+@dataclasses.dataclass
 class ImageMountWithArchive(ImageMount):
     archive_to: typing.Optional[PathType] = None
     archive_success: typing.Optional[PathType] = None
     archive_method: typing.Optional[str] = None
     archive_tar_content_owner: typing.Optional[str] = None
-
-
--- tests/test_archive_overlay.py
+++ tests/test_archive_overlay.py
@@ -1,20 +1,18 @@
-import asyncio.subprocess
 import pathlib
 
 import pytest
-
-from containers import BindMount
 from containers import Container
 from containers import ContainersService
-from containers import ImageMount
 from containers import make_containers_service
-from .providers import PodmanWithArchive
+
 from .data_types import ImageMountWithArchive
+from .providers import PodmanWithArchive
 
 
 @pytest.fixture
 def podman() -> PodmanWithArchive:
-    return make_containers_service()
+    return PodmanWithArchive()
+
 
 @pytest.fixture
 def containers(podman: PodmanWithArchive) -> ContainersService:
@@ -28,70 +26,20 @@ async def test_archive_with_copy(tmp_path: pathlib.Path, containers: ContainersS
 
     archive_to = tmp_path / "archive"
 
-    image_mount = ImageMount(source=data_image, target="/data")
-    container = Container(
-        command=("ls", "/data"), image="alpine:3.18.2", mounts=[image_mount]
+    image_mount = ImageMountWithArchive(
+        source=data_image,
+        target="/data",
+        read_write=True,
+        archive_to=archive_to,
     )
-    async with containers.run(container, stdout=asyncio.subprocess.PIPE) as proc:
-        stdout = await proc.stdout.read()
-        folders = frozenset(
-            filter(lambda item: bool(item.strip()), stdout.decode("utf8").split("\n"))
-        )
-        assert folders == frozenset(
-            {
-                "bin",
-                "dev",
-                "etc",
-                "home",
-                "lib",
-                "media",
-                "mnt",
-                "opt",
-                "proc",
-                "root",
-                "run",
-                "sbin",
-                "srv",
-                "sys",
-                "tmp",
-                "usr",
-                "var",
-            }
-        )
-        assert await proc.wait() == 0
-
-
-@pytest.mark.asyncio
-async def test_run_with_bind_mount(
-        tmp_path: pathlib.Path, containers: ContainersService
-):
-    data_image = "alpine:3.18.2"
-    await containers.load_image(data_image)
-
-    mount_dir = tmp_path / "data"
-    mount_dir.mkdir()
-    file0 = mount_dir / "my-file0.txt"
-    file0.write_text("hello")
-    file1 = mount_dir / "nested" / "my-file1.txt"
-    file1.parent.mkdir(parents=True, exist_ok=True)
-    file1.write_text("there")
-
-    bind_mount = BindMount(source=mount_dir, target="/data", readonly=False)
     container = Container(
-        command=("/bin/sh", "-c", "touch /data/new && ls /data"),
+        command=("/bin/sh", "-c", "echo hello > /data/file"),
         image="alpine:3.18.2",
-        mounts=[bind_mount],
+        mounts=[image_mount],
     )
-    async with containers.run(container, stdout=asyncio.subprocess.PIPE) as proc:
-        stdout = await proc.stdout.read()
-        folders = frozenset(
-            filter(lambda item: bool(item.strip()), stdout.decode("utf8").split("\n"))
-        )
-        assert folders == frozenset(
-            {
-                "my-file0.txt",
-                "nested",
-                "new",
-            }
-        )
+    async with containers.run(container, log_level="debug") as proc:
         assert await proc.wait() == 0
+
+    file_path = archive_to / "file"
+    assert file_path.exists()
+    assert file_path.read_text().strip() == "hello"
