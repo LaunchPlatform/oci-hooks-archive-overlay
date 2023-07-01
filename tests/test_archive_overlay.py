29b80da815dbffcf45a32bdecf33224c47ac410a
Fang-Pen Lin <hello@fangpenlin.com>
2023-06-30 20:47:02 -0700

    Add more tests

--- tests/test_archive_overlay.py
+++ tests/test_archive_overlay.py
@@ -1,4 +1,5 @@
 import pathlib
+import tarfile
 
 import pytest
 from containers import Container
@@ -44,3 +45,37 @@ async def test_archive_with_copy(tmp_path: pathlib.Path, containers: ContainersS
     file_path = archive_to / "file"
     assert file_path.exists()
     assert file_path.read_text().strip() == "hello"
+
+
+@pytest.mark.asyncio
+async def test_archive_with_targz(
+    tmp_path: pathlib.Path, containers: ContainersService
+):
+    data_image = "alpine:3.18.2"
+    await containers.load_image(data_image)
+
+    archive_to = tmp_path / "archive.tar.gz"
+
+    image_mount = ImageMountWithArchive(
+        source=data_image,
+        target="/data",
+        read_write=True,
+        archive_to=archive_to,
+        archive_method="tar.gz",
+    )
+    container = Container(
+        command=("/bin/sh", "-c", "echo hello > /data/file"),
+        image="alpine:3.18.2",
+        mounts=[image_mount],
+        network="none",
+    )
+    async with containers.run(container, log_level="debug") as proc:
+        assert await proc.wait() == 0
+
+    assert archive_to.exists()
+    with tarfile.open(archive_to, "r:gz") as tar_file:
+        members = {tar_info.name: tar_info for tar_info in tar_file.getmembers()}
+        file_name = "./file"
+        assert file_name in members
+        with tar_file.extractfile(members[file_name]) as fo:
+            assert fo.read().decode("utf8").strip() == "hello"
