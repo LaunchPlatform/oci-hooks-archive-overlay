e9a1308e4fd2f495b5a3dbaaa898f87f3eb26e9a
Fang-Pen Lin <hello@fangpenlin.com>
2023-06-30 20:49:34 -0700

    Add test for archive success

--- tests/test_archive_overlay.py
+++ tests/test_archive_overlay.py
@@ -79,3 +79,33 @@ async def test_archive_with_targz(
         assert file_name in members
         with tar_file.extractfile(members[file_name]) as fo:
             assert fo.read().decode("utf8").strip() == "hello"
+
+
+@pytest.mark.asyncio
+async def test_archive_success(tmp_path: pathlib.Path, containers: ContainersService):
+    data_image = "alpine:3.18.2"
+    await containers.load_image(data_image)
+
+    archive_to = tmp_path / "archive"
+    archive_success = tmp_path / "success"
+
+    image_mount = ImageMountWithArchive(
+        source=data_image,
+        target="/data",
+        read_write=True,
+        archive_to=archive_to,
+        archive_success=archive_success,
+    )
+    container = Container(
+        command=("/bin/sh", "-c", "echo hello > /data/file"),
+        image="alpine:3.18.2",
+        mounts=[image_mount],
+        network="none",
+    )
+    async with containers.run(container, log_level="debug") as proc:
+        assert await proc.wait() == 0
+
+    file_path = archive_to / "file"
+    assert file_path.exists()
+    assert file_path.read_text().strip() == "hello"
+    assert archive_success.read_text() == ""
